eca15abd7f94fa69204a3e053324f508d32c59c7
adolfosonchiche <sonadolfo36@gmail.com>
2025-02-13 17:42:07 -0600

    tests

---
+++ src/test/java/com/adolfosc/algoritmo/ArrayObjectTest.java
@@ -0,0 +1,45 @@
+
+package com.adolfosc.algoritmo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author adolfo-son
+ */
+public class ArrayObjectTest {
+    
+    public ArrayObjectTest() {
+    }
+
+    /**
+     * Test of obtainSingleArray method, of class ArrayObject.
+     */
+    @org.junit.jupiter.api.Test
+    public void testObtainSingleArray() {
+        System.out.println("obtainSingleArray");
+        Object[] arr = null;
+        Object[] result_2 = null;
+        ArrayObject instance = new ArrayObject();
+        Object[] expResult = null;
+        Object[] result = instance.obtainSingleArray(arr, result_2);
+        assertArrayEquals(expResult, result);
+    }
+
+    /**
+     * Test of transformArray method, of class ArrayObject.
+     */
+    @org.junit.jupiter.api.Test
+    public void testTransformArray() {
+        System.out.println("transformArray");
+        Object [] arr1 = new Object[] {1, 2, new Object[]{"3", "4"}};
+        //Object[] arr = null;
+        int contador = 0;
+        Object[] result_2 = new Object[arr1.length];
+        ArrayObject instance = new ArrayObject();
+        Object[] expResult = {1,2,"3","4"};
+        Object[] result = instance.transformArray(arr1, contador, result_2);
+        assertArrayEquals(expResult, result);
+    }
+    
+}
